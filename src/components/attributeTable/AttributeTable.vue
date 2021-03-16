49bd5f3fe2f499f5bf8a442347b94f032ad0c67b
Jakob Miksch <jakob@meggsimum.de>
2021-03-16 15:25:03 +0100

    WIP

--- src/components/attributeTable/AttributeTable.vue
+++ src/components/attributeTable/AttributeTable.vue
@@ -3,7 +3,7 @@
     v-if=show
     dense
     :headers="headers"
-    :items="desserts"
+    :items="features"
     item-key="name"
     class="elevation-1"
   ></v-data-table>
@@ -11,111 +11,48 @@
 
 <script>
 
-import { DraggableWin } from '../../directives/DraggableWin';
+import { Mapable } from '../../mixins/Mapable';
 
 export default {
   name: 'wgu-attributetable-win',
+  mixins: [Mapable],
 
-  directives: {
-    DraggableWin
+  methods: {
+    onMapBound: function () {
+      console.log('map ready in attribute table');
+    }
   },
 
   data: () => ({
     show: false,
-    desserts: [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: '1%'
-      },
-      {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: '1%'
-      },
-      {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: '7%'
-      },
-      {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: '8%'
-      },
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: '16%'
-      },
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: '0%'
-      },
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: '2%'
-      },
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: '45%'
-      },
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: '22%'
-      },
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: '6%'
-      }
-    ],
     headers: [
       {
-        text: 'Dessert (100g serving)',
+        text: 'Ort',
         align: 'start',
         sortable: false,
-        value: 'name'
+        value: 'NAME'
       },
-      { text: 'Calories', value: 'calories' },
-      { text: 'Fat (g)', value: 'fat' },
-      { text: 'Carbs (g)', value: 'carbs' },
-      { text: 'Protein (g)', value: 'protein' },
-      { text: 'Iron (%)', value: 'iron' }
+      { text: 'Einwohner (Max)', value: 'POP_MAX' }
+    ],
+    features: [
+      {
+        'NAME': 'Vatican City',
+        'ADM0NAME': 'Vatican (Holy See)',
+        'POP_MAX': 832,
+        'TIMEZONE': 'Europe/Vatican'
+      },
+      {
+        'NAME': 'San Marino',
+        'ADM0NAME': 'San Marino',
+        'POP_MAX': 29579,
+        'TIMEZONE': 'Europe/San_Marino'
+      },
+      {
+        'NAME': 'Vaduz',
+        'ADM0NAME': 'Liechtenstein',
+        'POP_MAX': 36281,
+        'TIMEZONE': 'Europe/Vaduz'
+      }
     ]
   })
 }
