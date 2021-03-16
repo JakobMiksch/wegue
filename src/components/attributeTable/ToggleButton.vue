9dc0410efc3896b0bc97fa97dfffa0de11d7669f
Jakob Miksch <jakob@meggsimum.de>
2021-03-16 14:51:02 +0100

    WIP: Attribute table

---
+++ src/components/attributeTable/ToggleButton.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <v-btn icon :dark="dark" @click="toggleUi">
+    <v-icon medium>{{icon}}</v-icon>
+    {{text}}
+  </v-btn>
+
+</template>
+
+<script>
+import Vue from 'vue'
+import { WguEventBus } from '../../WguEventBus'
+import AttributeTableWin from './AttributeTable';
+
+export default {
+  name: 'wgu-attributetable-btn',
+  components: {
+    'wgu-attributetable-win': AttributeTableWin
+  },
+  props: {
+    icon: {type: String, required: false, default: 'table_chart'}, // table_chart, view_comfy
+    text: {type: String, required: false, default: ''},
+    dark: {type: Boolean, required: false, default: false}
+  },
+  data: function () {
+    return {
+      moduleName: 'wgu-attributetable'
+    }
+  },
+  created () {
+    var me = this;
+    // TODO move to a father class
+    WguEventBus.$on('app-mounted', () => {
+      me.win = Vue.prototype.cmpLookup[me.moduleName + '-win'];
+    });
+    if (!me.win) {
+      me.win = Vue.prototype.cmpLookup[me.moduleName + '-win'];
+    }
+  },
+  methods: {
+    toggleUi () {
+      // TODO move to a father class
+      this.win.show = !this.win.show;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
